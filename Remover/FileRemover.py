28a6f6653104bd5c5e42c712fac6f1c6b97114e7
SauhardoSengupta <70318077+sauhardo2020@users.noreply.github.com>
2021-04-11 13:25:28 +0530

    Add files via upload

---
+++ Remover/FileRemover.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import time
+
+File_Path_Input = input("Please Enter File Path")
+
+S = time.time()
+
+if(os.path.exists(File_Path_Input)):
+         
+    FileAndFolder = os.walk(File_Path_Input)
+
+    AllFiles = os.path.join(str(File_Path_Input) + '/' + str(FileAndFolder))
+    C_Time_1 = os.stat(File_Path_Input).st_ctime
+
+    if(C_Time_1 < S):
+        shutil.rmtree(File_Path_Input)
+        print("Printed Message")
+else:
+     print("Sorry Path not found")   
+   
