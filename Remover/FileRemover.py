2c0d92aa7699b8def8e877cbbac36f57c92074e5
SauhardoSengupta <70318077+sauhardo2020@users.noreply.github.com>
2021-05-04 13:22:26 +0530

    Update FileRemover.py

--- Remover/FileRemover.py
+++ Remover/FileRemover.py
@@ -3,19 +3,17 @@ import shutil
 import time
 
 File_Path_Input = input("Please Enter File Path")
-
 S = time.time()
 
 if(os.path.exists(File_Path_Input)):
-         
+
     FileAndFolder = os.walk(File_Path_Input)
 
     AllFiles = os.path.join(str(File_Path_Input) + '/' + str(FileAndFolder))
     C_Time_1 = os.stat(File_Path_Input).st_ctime
 
-    if(C_Time_1 < S):
+    if(C_Time_1 < 30):
         shutil.rmtree(File_Path_Input)
-        print("Printed Message")
+        print("Done")
 else:
-     print("Sorry Path not found")   
-   
+    print("Sorry Path not found")
